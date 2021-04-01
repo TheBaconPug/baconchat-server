a94ec47d25104d42a0c3a4fee8507bf7810b8445
TheBaconPug <thebaconpug@gmail.com>
2021-04-01 03:29:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.Server(app);
+
+const sockets = require('socket.io');
+io = sockets(server);
+
+
+io.on('connection', function (connection) {
+    connection.on('message', function(data) {
+        console.log(data);
+        io.emit("broadcast", data);
+    });
+});
+
+app.get('/', (req, res) => {
+  res.send(`The server has been started`)
+});
+
+server.listen(3000, function() {
+  console.log('The server has started');
+});
